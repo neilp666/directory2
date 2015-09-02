70c70ef88439487623b512ec8148c242cdacd954
neil patel <neil@myindianweddingapp.com>
2015-09-02 14:38:41 +0100

    added carrierwave gem

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -69,6 +69,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:title, :email, :company, :location, :telephone, :website, :description)
+      params.require(:profile).permit(:title, :email, :company, :location, :telephone, :website, :description, :image)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150902105630) do
+ActiveRecord::Schema.define(version: 20150902132424) do
 
   create_table "profiles", force: true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150902105630) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "users", force: true do |t|
